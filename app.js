3ff692e5bdd82ea8742b3e73a032804bec55e9ea
shubham <shubham0jangid@gmail.com>
2022-04-05 23:16:51 +0530

    Initial Update

--- app.js
+++ app.js
@@ -136,10 +136,11 @@ app.post("/delete", function (req, res) {
 app.get("/about", function (req, res) {
 	res.render("about");
 });
+
 let port = process.env.PORT;
 if (port == null || port == "") {
 	port = 3000;
 }
 app.listen(port, function () {
-	console.log("Server started on port 3000");
+	console.log("Server started Successfully");
 });
