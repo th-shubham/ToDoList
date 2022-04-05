abfe987ef8adaf8cb39e0e86c993d792135e900b
shubham <shubham0jangid@gmail.com>
2022-04-05 21:15:41 +0530

    Database Added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const mongoConnectionURL = process.env.MONGO_URI;
 
 const app = express();
 
@@ -12,8 +13,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+// console.log(process.env.MONGO_URI);
+mongoose.connect(mongoConnectionURL, {
 	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 const itemsSchema = {
